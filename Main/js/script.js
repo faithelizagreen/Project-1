438ee23b0c77035269f33ede00bb2c0a5326beb8
Jessica Galarza <jessicaga941@gmail.com>
2021-10-05 14:58:52 -0400

    worked on Modal and script

--- Main/js/script.js
+++ Main/js/script.js
@@ -2,11 +2,40 @@ var npsKey = 'jWkeYbbVnmq03Oy2hhCYC6rXV7GUQlVtL46HooYG';
 var rootUrl = 'https://developer.nps.gov/api/v1';
 var stateName = 'FL';
 
+
 var queryURL = 'https://developer.nps.gov/api/v1/parks?stateCode=' + stateName + "&api_key=" + npsKey;
 fetch(queryURL)
-    .then(function (response) {
+    .then(function(response) {
         return response.json();
     })
-    .then(function (data) {
+    .then(function(data) {
         console.log(data);
     })
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
