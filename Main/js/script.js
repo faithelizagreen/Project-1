2b3b588a5e20723d369cff35c1ecf0e207c4e7f1
Leah Davis <ileahd96@gmail.com>
2021-10-05 14:51:44 -0400

    HTML Built to spec and placeholders created. Google Map imported as well but non-functioning.

--- Main/js/script.js
+++ Main/js/script.js
@@ -10,3 +10,10 @@ fetch(queryURL)
     .then(function (data) {
         console.log(data);
     })
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
